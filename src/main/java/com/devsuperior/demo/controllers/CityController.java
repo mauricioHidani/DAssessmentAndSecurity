f6ca4e77434bdcee88dde1d453fb418d5c50ab48
Mauricio Hidani <mauricio.hidani.work@gmail.com>
2023-10-30 15:53:21 -0300

    feat: city insert data method

--- src/main/java/com/devsuperior/demo/controllers/CityController.java
+++ src/main/java/com/devsuperior/demo/controllers/CityController.java
@@ -4,15 +4,15 @@ import com.devsuperior.demo.dto.CityDTO;
 import com.devsuperior.demo.services.CityService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequestMapping("/cities")
@@ -31,6 +31,13 @@ public class CityController {
                 .buildAndExpand(result.getId())
                 .toUri();
         return ResponseEntity.created(uri).body(result);
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<CityDTO>> findAll(Pageable pageable) {
+        return ResponseEntity.ok(
+                service.findAll(pageable)
+        );
     }
 
 }
---
+++ src/main/java/com/devsuperior/demo/services/CityService.java
@@ -0,0 +1,30 @@
+package com.devsuperior.demo.services;
+
+import com.devsuperior.demo.dto.CityDTO;
+import com.devsuperior.demo.entities.City;
+import com.devsuperior.demo.repositories.CityRepository;
+import com.devsuperior.demo.services.exceptions.NotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class CityService {
+
+    @Autowired
+    private CityRepository repository;
+
+    @Transactional
+    public CityDTO insert(CityDTO city) {
+        City saved = repository.save(new City(null, city.getName()));
+        return new CityDTO(saved);
+    }
+
+}
