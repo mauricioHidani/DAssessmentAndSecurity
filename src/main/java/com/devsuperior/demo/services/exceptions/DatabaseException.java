6bb6de998c5ea0fd779350bcc60ca05234775b43
Mauricio Hidani <mauricio.hidani.work@gmail.com>
2023-10-30 12:10:08 -0300

    feat: database intercept exception on services

---
+++ src/main/java/com/devsuperior/demo/services/exceptions/DatabaseException.java
@@ -0,0 +1,9 @@
+package com.devsuperior.demo.services.exceptions;
+
+public class DatabaseException extends RuntimeException {
+
+    public DatabaseException(String message) {
+        super(message);
+    }
+
+}
